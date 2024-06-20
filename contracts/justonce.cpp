1c4a00b7ebaa50784d00b5bf3ba9e6bc9e1dc4ef
C. Harrison <cfharr@gmail.com>
2024-06-20 12:01:49 -0700

    limit purge count, new abi, eventid is hash with scope

--- contracts/justonce.cpp
+++ contracts/justonce.cpp
@@ -1,4 +1,5 @@
 #include "justonce.hpp"
+#include <eosio/crypto.hpp>
 
 //contractName:justonce
 
@@ -9,29 +10,37 @@ void justonce::reset() {
   }
 }
 
-void justonce::newevent( name ram_payer, uint64_t eventid, uint64_t lifetime_sec ) {
+void justonce::antirepeat( uint64_t nonce, uint64_t lifetime_sec,
+     name scope, name ram_payer ) {
   require_auth(ram_payer);
   check( lifetime_sec >= min_lifetime && lifetime_sec <= max_lifetime,
          "invalid lifetime");
          
   // retire expired events
   // loop through event table starting from earliest expiration time
+  // limit number of items removed to max_purges
   const uint32_t time_now = current_time_point().sec_since_epoch();
   auto expidx = events.get_index<"byexpiry"_n>();
+  int i = max_purges;
   for (auto exp = expidx.cbegin(), end = expidx.cend();
-      exp != end && exp->expiry < time_now;
+      exp != end && exp->expiry < time_now && --i > 0 ;
       exp = expidx.erase(exp)) {}
   
   // emplace new event
-  auto itr = events.find(eventid);
-  if (itr == events.end() ) {
-    events.emplace(ram_payer, [&](auto& s) {
-      s.event_id = eventid;
-      s.expiry = time_now + lifetime_sec;
-    });
-  } else {
-    check(false, "repeated event");
-  }
+  const struct {
+    uint64_t n;
+    uint64_t s;
+  } data = {nonce, scope.value};
+  checksum160 sha1hash = sha1((const char *)&data, sizeof(data));
+  // event_id is first 8 bytes of sha1 hash
+  uint64_t event_id = * (uint64_t*)&sha1hash;
+  auto itr = events.find(event_id);
+  check(itr == events.end(), "repeated event");
+  
+  events.emplace(ram_payer, [&](auto& s) {
+    s.event_id = event_id;
+    s.expiry = time_now + lifetime_sec;
+  });
 }
   
 
--- contracts/justonce.hpp
+++ contracts/justonce.hpp
@@ -10,11 +10,16 @@ using std::string;
     * customer buys something by scanning a QR code containing a `transfer` action.
     *
     * The merchant computes a nonce (which could be an invoice number, or a hash of the transaction
-    * details) and appends a `justonce` `newevent` action to the sales transaction. This action
-    * places the nonce ("event id") into a history table.
+    * details) and appends a `justonce` `antirepeat` action to the sales transaction. This action
+    * places the event into a history table. The event_id in the table is a hash of the nonce and the
+    * scope field.
     *
-    * If the customer scans the same QR code a second time, the `newevent` action will fail with
+    * If the customer scans the same QR code a second time, the `antirepeat` action will fail with
     * a "repeated event" error.
+    *
+    * A repeated event is detected if the nonce and scope match a history table entry. The merchant
+    * is expected to use their account name as scope, and merchant is responsible for ensuring that the
+    * nonces are sufficiently unique within that scope.
     *
     * In order to limit the history table size, each event action submission includes a lifetime parameter;
     * events are purged from the table after that lifetime.
@@ -30,12 +35,14 @@ CONTRACT justonce : public contract {
 
         ACTION reset();
 
-        ACTION newevent( name ram_payer, uint64_t eventid, uint64_t lifetime_sec );
+        ACTION antirepeat( uint64_t nonce, uint64_t lifetime_sec,
+           name scope, name ram_payer );
 
     private:
 
         static const uint32_t min_lifetime = 1;
         static const uint32_t max_lifetime = 2*31536000; // 2 years
+        static const int max_purges = 10; // to protect against execution timeout
         
         TABLE event_ids {
             uint64_t    event_id;
